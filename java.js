d8593919df4f494b966905b31e93e028b606de7f
Zeldoux <161885997+Zeldoux@users.noreply.github.com>
2024-04-29 17:06:11 +0000

    test

--- java.js
+++ java.js
@@ -3,7 +3,7 @@
 // get the navbar
 const navbar = document.querySelector(".navbar");
 window.addEventListener("scroll",function() {
-    if (this.document.documentElement.scrollTop > 10) {
+    if (this.document.documentElement.scrollTop > 1) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky")
