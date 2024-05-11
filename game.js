38329aca7c64d35233ac3c787eef2c059b8819f5
Zeldoux <161885997+Zeldoux@users.noreply.github.com>
2024-05-11 15:44:19 +0000

    correct img loading conflict

--- game.js
+++ game.js
@@ -4,15 +4,40 @@ console.log("hello world");
 
 
 let img;
+let imgall={};
+let speed={};
 
 function load() {
-    img = new Sprite("img/Player.png", 1, 1 );
+    let px = 0;
+    let py = 0;
+    for (i= 0 ; i < 10 ; i++) {
+
+        speed[i] = Math.random(10,100);
+        imgall[i] = new Sprite("img/Player.png",px,py);
+        py = py + 50;
+
+    }
+
+    img = new Sprite("img/Player.png");
+    
+
 }
 
 function update() {
 
+    for (i = 0 ; i < 10 ; i++) {
+        imgall[i].x = imgall[i].x + speed[i] % 10; 
+
+    }
+    
+
 }
 
 function draw(pCtx) { // pCtx = parameter context
+
     img.draw(pCtx);
+    for (i= 0 ; i < 10 ; i ++) {
+        imgall[i].draw(pCtx);
+    }
+
 }
