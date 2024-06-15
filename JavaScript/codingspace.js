11c35d4d1258a6db13f7526405da093b31a140c3
Yoann Sousa <yoannsousa@laposte.net>
2024-06-15 16:18:25 +0200

    many change and fix added profile page almost finish

--- JavaScript/codingspace.js
+++ JavaScript/codingspace.js
@@ -1,8 +1,9 @@
 require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.0/min/vs' } });
+//import { Terminal } from '../node_modules/@xterm/xterm';
 
 
 // put monaco library vs code into editor-container and config vs code style //
-require(['vs/editor/editor.main','https://cdnjs.cloudflare.com/ajax/libs/xterm/4.15.0/xterm.min.js'], function() {
+require(["vs/editor/editor.main"], function() {
     monaco.editor.create(document.getElementById('editor-container'), {
         value:[
             'function helloWorld() {',
@@ -13,12 +14,13 @@ require(['vs/editor/editor.main','https://cdnjs.cloudflare.com/ajax/libs/xterm/4
             theme: 'vs-dark',
             automaticLayout: true
     });
-        // load and create terminal using xterm.js //
-    const terminalContainer = document.getElementById("terminal");
-    const terminal = new Terminal();
-    terminal.open(terminalContainer);
-
-    terminal.write("Welcome to the Terminal\r\n");
 });
+
+// load and create terminal using xterm.js //
+//const terminalContainer = document.getElementById("terminal");
+//const terminal = new Terminal();
+//terminal.open(terminalContainer);
+    
+//terminal.write("Welcome to the Terminal\r\n");
 
 
